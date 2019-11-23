8ba84138b3bea94cfe4098f0f08e0be4a3d15c25
Alan He <wohezhuohao@outlook.com>
2019-11-23 14:39:55 +0800

    更新注释
    更加完善的注释

--- Lab/Prime.asm
+++ Lab/Prime.asm
@@ -1,35 +1,38 @@
-assume cs:code
+.model small
+.386
 
-code segment
+.code
 
 start:
 ;main faction
     mov ax,2
 NEXT:
-    mov cx,ax
+    mov ecx,eax
     mov bx,2
     ;ax是被除数 1~10000
     ;bx是除数
 s:  
-    mov ax,cx
-    cmp bx,ax   ;bx是否等于ax
+    mov eax,ecx
+    cmp ebx,eax   ;bx是否等于ax
     jz L2       ;如果等于则转到L2
-    mov dx,0    ;把dx清空
-    div bx      ;ax除以bx，ax存商，dx存余数
-    cmp dx,0    ;bx是否整除dx
+    mov edx,0    ;把dx清空
+    div ebx      ;ax除以bx，ax存商，dx存余数
+    cmp edx,0    ;bx是否整除dx
     jz L1       ;如果整除则转到L1
     inc bx      ;bx++
     cmp bx,101  ;如果bx到100还没有整除ax
     jz L2       ;转到L2，即ax是素数
     jmp s       ;跳到s，进行循环
 L1:
-    mov ax,cx   ;把cx赋值给ax,cx中存放的是原来的ax，即被除数
-    cmp ax,10000;判断是否到达一万
+    mov eax,ecx   ;把cx赋值给ax,cx中存放的是原来的ax，即被除数
+    cmp eax,100000;判断是否到达一万
     jz L7       ;如果到达一万则进行收尾工作L7
-    inc ax      ;没达到一万就ax++
+    inc eax      ;没达到一万就ax++
     jmp NEXT    ;回到主语句
 L2:;输出素数（先行判断，确定ax的范围）
-    mov ax,cx
+    mov eax,ecx
+    cmp eax,10000
+    jnb L8
     cmp ax,1000
     jnb L3      ;若ax>=1000则转L3
     cmp ax,100
@@ -37,6 +40,16 @@ L2:;输出素数（先行判断，确定ax的范围）
     cmp ax,10
     jnb L5      ;若10<=ax<100则转L5
     jmp L6      ;若ax<10则转L6
+L8:
+    mov dx,0
+    mov bx,10000
+    div ebx
+    mov ebx,edx
+    mov dl,al
+    add dl,30H
+    mov ah,2H
+    int 21H
+    mov ax,bx
 L3: ;ax>=1000
     mov dx,0
     mov bx,1000
@@ -80,6 +93,6 @@ L6:  ;ax<10
 L7:
     mov ax,4c00H
     int 21H
-code ends
+
 
 end start
--- Lab/fibo.asm
+++ Lab/fibo.asm
@@ -3,18 +3,24 @@
 
 .code
 
+;计算斐波那契数列
+
 start:
     mov eax,1
+    ;输入想要计算的位数
     mov cx,47
 s:  
+    ;eax入栈，并调用fibo函数
     push eax
     call fibo
-
+    ;调用打印输出函数
     call printf
 
+    ;eax出栈
     pop eax
+    ;eax = eax + 1
     inc eax
-
+    ;循环，计算下一位斐波那契数
     loop s
 
 
@@ -22,33 +28,45 @@ s:
     int 21H
 
 fibo:
+;    if(eax == 1 || eax == 2)
+;       {
+;           eax = 1
+;           return ;              
+;       }
+
     cmp eax,1
     je _get_out
     cmp eax,2
     je _get_out
 
-    ;todo:如果 ax == 1,则直接跳出函数，返回
+;   把eax,ebx,ecx入栈，防止干扰
     push ebx
     push ecx
     push edx
+;开始计算斐波那契数 递归
+    mov edx,eax
+;   把eax存放在eda  
 
-    mov edx,eax  
+;   ebx = eax = fibo(n-1)    
     sub eax,1
     call fibo
     mov ebx,eax
 
+;   ecx = eax = fibo(n-2)
     mov eax,edx
     sub eax,2
     call fibo
     mov ecx,eax
 
     mov eax,ebx
+;   fibo(n) = fibo(n-1) + fibo(n-2)    
     add eax,ecx
+
 
     pop edx
     pop ecx
     pop ebx
-
+;返回函数
     ret
 
     
@@ -59,25 +77,34 @@ _get_out:
 printf:
 ;eax<=‭      4294967296
 ; 47    2971215073
+    ;若1000000000<=ax<=10000000000则转pt11
     cmp eax,1000000000
     jnb pt11
     ; 36 ->  14930352
+    ;若100000000<=ax<=1000000000则转pt10
     cmp eax,100000000
     jnb pt10
+    ;若10000000<=ax<=100000000则转pt9
     cmp eax,10000000
     jnb pt9
+    ;若1000000<=ax<=10000000则转pt8
     cmp eax,1000000
     jnb pt8
+    ;若100000<=ax<=1000000则转pt7 
     cmp eax,100000
     jnb pt7
+    ;若10000<=ax<=100000则转pt6 
     cmp eax,10000
-    jnb pt6
+    jnb pt6    
+    ;若1000<=ax<=10000则转pt5 
     cmp ax,1000
-    jnb pt5      ;若ax>=1000则转L3
+    jnb pt5      
+    ;若100<=ax<1000则转pt4
     cmp ax,100
-    jnb pt4      ;若100<=ax<1000则转L4
+    jnb pt4    
+    ;若10<=ax<100则转pt3  
     cmp ax,10
-    jnb pt3      ;若10<=ax<100则转L5
+    jnb pt3      
     jmp pt2  
 ;100,000,000<ax<=1,000,000,000\
 
@@ -155,19 +182,19 @@ pt6:
 pt5:    
     mov edx,0
     mov ebx,1000
-    div ebx      ;ax除以1000，商ax余dx
-    mov ebx,edx   ;把余数存放在bx中
+    div ebx      
+    mov ebx,edx  
     mov dl,al
-    add dl,30H  ;输出空格
+    add dl,30H  
     mov ah,2H
     int 21H     ;输出千位数
     mov eax,ebx
 ;100<ax<=1000
 pt4:
     mov bl,100  
-    div bl      ;al除以100
-    mov bl,ah   ;余数ah存放在bl中
-    mov dl,al   ;商al存放在dl中
+    div bl      
+    mov bl,ah   
+    mov dl,al   
     add dl,30H
     mov ah,2H
     int 21H     ;输出百位数dl
@@ -176,9 +203,9 @@ pt4:
 ;10<ax<=100
 pt3:
     mov bl,10   
-    div bl      ;bl除以10
-    mov bl,ah   ;余数ah存放在bl中
-    mov dl,al   ;商al存放在dl中
+    div bl      
+    mov bl,ah   
+    mov dl,al   
     add dl,30H
     mov ah,2H   
     int 21H     ;输出十位数dl
